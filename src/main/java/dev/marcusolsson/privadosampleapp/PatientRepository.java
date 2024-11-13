6756213e071f84b788908f0428decc29a45d7319
Marcus Olsson <8396880+marcusolsson@users.noreply.github.com>
2024-11-13 20:59:04 +0100

    Remove repository and controller

--- src/main/java/dev/marcusolsson/privadosampleapp/PatientRepository.java
+++
@@ -1,6 +0,0 @@
-package dev.marcusolsson.privadosampleapp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
